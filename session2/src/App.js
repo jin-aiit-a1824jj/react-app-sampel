853290bef38c2b1bdd222fc35a2ec21a181912c1
JIN <a1824jj@aiit.ac.jp>
2020-06-24 08:12:53 +0000

    prop-types

--- session2/src/App.js
+++ session2/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = ()=>{
 
   const profiles = [
     {name: "Taro", age: 10},
     {name: "Hanako", age: 5},
-    {name: "NoName"}
+    {name: "NoName", age: 3}
   ]
 
   return(
@@ -16,9 +17,6 @@ const App = ()=>{
           return <User name={profile.name} age={profile.age} key={index}/>
         })
       }
-
-      {/* <User name={"Taro"} age={10}/>
-      <User name={"Hanako"} age={5}/> */}
     </div>
   );
 }
@@ -27,8 +25,9 @@ const User = (props)=> {
 return <div>Hi, I am {props.name}, and {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
