327823a9e796bfca5ccbfb390d96910240b20b3d
JIN <a1824jj@aiit.ac.jp>
2020-06-24 08:00:07 +0000

    create class component and functional component

--- session2/src/App.js
+++ session2/src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
-function App() {
-  // const greeting = "Hi!"
-  // const dom = <h1 className="foo">{greeting}</h1>;
+// function App() {
+//   // const greeting = "Hi!"
+//   // const dom = <h1 className="foo">{greeting}</h1>;
   
-  return (
+//   return (
+//     <div>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onClick={()=>{console.log("I am clicked! ")}}/>
+//     </div>
+//   );
+// }
+
+const App = ()=>{
+  return(
     <div>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={()=>{console.log("I am clicked! ")}}/>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+      <Cat/>
     </div>
   );
 }
 
-
+const Cat = ()=> {
+  return <div>Meow!</div>
+}
 
 export default App;
