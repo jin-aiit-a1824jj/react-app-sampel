4833a62683fbae238611080c650b9284ec040f9e
JIN <a1824jj@aiit.ac.jp>
2020-06-24 08:07:43 +0000

    props of component

--- session2/src/App.js
+++ session2/src/App.js
@@ -1,30 +1,34 @@
 import React from 'react';
 
-// function App() {
-//   // const greeting = "Hi!"
-//   // const dom = <h1 className="foo">{greeting}</h1>;
-  
-//   return (
-//     <div>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onClick={()=>{console.log("I am clicked! ")}}/>
-//     </div>
-//   );
-// }
-
 const App = ()=>{
+
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 5},
+    {name: "NoName"}
+  ]
+
   return(
     <div>
-      <Cat/>
-      <Cat/>
-      <Cat/>
-      <Cat/>
+
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+
+      {/* <User name={"Taro"} age={10}/>
+      <User name={"Hanako"} age={5}/> */}
     </div>
   );
 }
 
-const Cat = ()=> {
-  return <div>Meow!</div>
+const User = (props)=> {
+return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
