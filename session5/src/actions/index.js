31d8932b9be8527c3efbcfb5f75db24760ed3dc9
JIN <a1824jj@aiit.ac.jp>
2020-06-27 09:02:39 +0000

    implement evnet new component

--- session5/src/actions/index.js
+++ session5/src/actions/index.js
@@ -1,12 +1,18 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENTS = 'CREATE_EVENTS'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
 
-export const readEvents = () =>  async dispatch => {
+export const readEvents = () => async dispatch => {
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
   //console.log(response)
   dispatch({ type: READ_EVENTS, response })
+};
+
+export const postEvents = value => async dispatch => {
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, value)
+  dispatch({ type: CREATE_EVENTS, response })
 };
