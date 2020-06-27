e9c964bee2ca79e7fd7b22bd1bbe03fac7567cd0
JIN <a1824jj@aiit.ac.jp>
2020-06-27 08:44:21 +0000

    using reduxForm and decorating

--- session5/src/components/events_new.js
+++ session5/src/components/events_new.js
@@ -2,22 +2,44 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import {Field, reduxForm } from 'redux-form';
+
 //import { postEvents } from '../actions';
 
 
 class EventsNew extends Component {
 
+  renderField(field){
+    const { input, label, type, meta: {touched, error}} = field
+    return (<div></div>)
+  }
+
   render() {
     
     return (
-      <React.Fragment>
-        <div>foo</div>
-      </React.Fragment>
-      
+      <form>
+        <div>
+          <Field label="Title" name="title" type="text" component={this.renderField}/>
+        </div>
+        <div>
+          <Field label="Body" name="body" type="text" component={this.renderField}/>
+        </div>
+        <div>
+          <input type="submit" value="Submit" disabled={false}></input>
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
     )
   }
 }
 
 //const mapDispatchToProps = ({ postEvents })
 
-export default connect(null, null)(EventsNew)
+const validate = values => {
+  const errors = {}
+  return errors
+}
+
+export default connect(null, null)(
+  reduxForm({validate, form: 'eventNewForm'})(EventsNew)
+)
